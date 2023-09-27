4ed1d43b522120d7a9f3ff3d3e4c9c5c6fc0881b
Tsuplige <thomassupligeon@gmail.com>
2023-09-27 23:44:40 +0200

    model : project, contributor, issue

--- SoftDesk_API/softdesk_app/models.py
+++ SoftDesk_API/softdesk_app/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Project(models.Model):
+
+    class Type(models.TextChoices):
+        BACK_END = "Back-end"
+        FRONT_END = "Front-end"
+        IOS = "IOS"
+        ANDROID = "Android"
+
+    description = models.fields.CharField(max_length=1000)
+    type = models.fields.CharField(choices=Type.choices, max_length=10)
+
+
+class Contributor(models.Model):
+
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name='working')
+
+    project = models.ForeignKey(to=Project, related_name='work_on')
+
+
+class Issue(models.Model):
+
+    class Priority(models.TextChoices):
+        HIGH = "High"
+        MEDIUM = "Medium"
+        LOW = "Low"
+
+    class Balise(models.TextChoices):
+        BUG = "Bug"
+        FEATURE = "Feature"
+        TASK = "Task"
+
+    class Status(models.TextChoices):
+        TO_DO = "To Do"
+        IN_PROGRESS = "In Progress"
+        FINISHED = "Finished"
+
+    project = models.ForeignKey(to=Project)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    contributor = models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True)
+
+
+class Comment(models.Model):
+    issue = models.ForeignKey(to=Issue)
